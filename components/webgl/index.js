3f7fbb1b8742ac5623d2aa714349365be4e34beb
Manuel Ruiz Redondo <manuelruizredondo@gmail.com>
2025-05-03 12:57:57 +0200

    cambios varios

--- components/webgl/index.js
+++ components/webgl/index.js
@@ -6,9 +6,15 @@ import { animated, useSpring } from '@react-spring/three'
 
 function SimpleIphoneModel({ steps, currentStep }) {
   const { scene } = useGLTF('/models/iphone/scene.gltf')
-  const texture = useTexture('/images/foto.png')
+  const textures = [
+    useTexture('/models/iphone/textures/foto1.png'),
+    useTexture('/models/iphone/textures/foto2.png'),
+    useTexture('/models/iphone/textures/foto3.png'),
+    useTexture('/models/iphone/textures/foto4.png'),
+    useTexture('/models/iphone/textures/foto5.png'),
+    useTexture('/models/iphone/textures/foto6.png'),
+  ]
 
-  // Configuramos el spring para posición, escala y rotación
   const { position, scale, rotation } = useSpring({
     position: steps[currentStep].position,
     scale: Array(3).fill(steps[currentStep].scale),
@@ -17,17 +23,21 @@ function SimpleIphoneModel({ steps, currentStep }) {
   })
 
   useEffect(() => {
-    // Con GLTFLoader las UVs vienen volteadas: ajustamos
-    texture.flipY = false
+    const emissiveTexture = currentStep % 4 === 0 ? textures[1] : textures[0]
+    emissiveTexture.flipY = false
 
-    // Cambiamos la textura del material_1 a la foto
+    console.log(`Cambiando a la textura: ${emissiveTexture.image.src}`)
+
     scene.traverse((child) => {
-      if (child.isMesh && child.material.name === 'material_1') {
-        child.material.map = texture
+      if (child.isMesh) {
+        if (child.material.map) {
+          child.material.map.dispose()
+        }
+        child.material.map = emissiveTexture
         child.material.needsUpdate = true
       }
     })
-  }, [scene, texture])
+  }, [scene, textures, currentStep])
 
   return (
     <animated.primitive
@@ -41,59 +51,12 @@ function SimpleIphoneModel({ steps, currentStep }) {
 
 export default function SimpleIphoneDisplay() {
   const steps = [
-    { position: [-0.1, -1.75, 0],    scale: 0.05,
-      rotation: [
-        MathUtils.degToRad(0),
-        MathUtils.degToRad(45),
-        MathUtils.degToRad(0),
-      ],
-
-     },
-    {
-      position: [5, -1.75, 0],
-      scale: 0.05,
-      rotation: [
-        MathUtils.degToRad(-0),
-        MathUtils.degToRad(150),
-        MathUtils.degToRad(-0),
-      ],
-    },
-    {
-      position: [5, -1.75, 0],
-      scale: 1.6,
-      rotation: [
-        MathUtils.degToRad(0),
-        MathUtils.degToRad(-180),
-        MathUtils.degToRad(0),
-      ],
-    },
-    {
-      position: [-0.2, -0.35, 0],
-      scale: 0.5,
-      rotation: [
-        MathUtils.degToRad(-90),
-        MathUtils.degToRad(-100),
-        MathUtils.degToRad(-45),
-      ],
-    },
-    {
-      position: [-1.2, -0.6, 0],
-      scale: 0.05,
-      rotation: [
-        MathUtils.degToRad(-90),
-        MathUtils.degToRad(-405),
-        MathUtils.degToRad(-45),
-      ],
-    },
-    {
-      position: [-1.6, -0.6, 0],
-      scale: 0.05,
-      rotation: [
-        MathUtils.degToRad(-90),
-        MathUtils.degToRad(-405),
-        MathUtils.degToRad(-45),
-      ],
-    },
+    { position: [-0.1, -3, 0], scale: 40, rotation: [0, MathUtils.degToRad(180), 0] },
+    { position: [5, -3, 0], scale: 40, rotation: [0, MathUtils.degToRad(90), 0] },
+    { position: [0, -3, 0], scale: 40, rotation: [0, MathUtils.degToRad(-180), 0] },
+    { position: [-0.2, -0.35, 0], scale: 40, rotation: [MathUtils.degToRad(-90), MathUtils.degToRad(-100), MathUtils.degToRad(-45)] },
+    { position: [-1.2, -0.6, 0], scale: 40, rotation: [MathUtils.degToRad(-90), MathUtils.degToRad(-405), MathUtils.degToRad(-45)] },
+    { position: [-1.6, -0.6, 0], scale: 40, rotation: [MathUtils.degToRad(-90), MathUtils.degToRad(-405), MathUtils.degToRad(-45)] },
   ]
 
   const [currentStep, setCurrentStep] = useState(0)
@@ -105,10 +68,7 @@ export default function SimpleIphoneDisplay() {
         const rect = section.getBoundingClientRect()
         const sectionHeight = rect.bottom - rect.top
 
-        if (
-          rect.top < window.innerHeight - sectionHeight / 2 &&
-          rect.bottom >= sectionHeight / 2
-        ) {
+        if (rect.top < window.innerHeight - sectionHeight / 2 && rect.bottom >= sectionHeight / 2) {
           setCurrentStep(index)
         }
       })
@@ -119,13 +79,9 @@ export default function SimpleIphoneDisplay() {
   }, [])
 
   return (
-    <Canvas
-      camera={{ position: [0, 0, 10], fov:50, near: 0.1, far: 100 }}
-    >
-      <ambientLight intensity={10} />
-      <directionalLight position={[5, 5, 5]} intensity={2} />
-
- 
+    <Canvas camera={{ position: [0, 0, 10], fov: 50, near: 0.1, far: 100 }}>
+      <ambientLight intensity={1} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
       <SimpleIphoneModel steps={steps} currentStep={currentStep} />
     </Canvas>
   )
